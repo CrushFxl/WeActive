abc8b16dc81e29fa05dd6fb5b43af403df5d8718
CrushFxl <2428977170@qq.com>
2023-10-17 20:00:20 +0800

    整合配置信息到config.ini文件流

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import time
 import selenium
 from selenium.webdriver.common.by import By
 from cv2 import imread, QRCodeDetector
+from configparser import ConfigParser
 
 
 def ScanQRCode(path):
@@ -12,25 +13,27 @@ def ScanQRCode(path):
     return info
 
 
-def MatchAnswer(text):
-    if "班级" in text:
-        return "智医2301"
-    elif "姓名" in text:
-        return "冯洋一"
-    elif "学号" in text:
-        return "6401230103"
+def GetAnswer(text):
+    conf = ConfigParser()
+    conf.read('config.ini', encoding='utf-8')
+    for key, value in conf['answer'].items():
+        if key in text:
+            return value
+    if conf.has_option("answer", "default"):
+        return conf["answer"]["default"]
     else:
-        return "非常好活动，爱来自曲折的江"
-
+        return False
 
 def FillForm(que):
-    num = que.get_attribute('topic')
+    # num = que.get_attribute('topic')
     text = que.find_element(By.CSS_SELECTOR, '[class="topichtml"]').text
     type = que.find_elements(By.XPATH, 'child::*')[1].get_attribute('class')
     if type == "ui-input-text":
-        ans = MatchAnswer(text)
-        que.find_element(By.TAG_NAME, 'input').send_keys(ans)
-        # print(a)
+        ans = GetAnswer(text)
+        if ans:
+            que.find_element(By.TAG_NAME, 'input').send_keys(ans)
+        else:
+            print("【WARNING】无有效匹配项且无缺省匹配，已跳过填写。")
     else:
         print("【WARNING】遇到未知的表单类型，已跳过填写。")
 
@@ -38,8 +41,8 @@ def FillForm(que):
 if __name__ == "__main__":
     cnt = 1
     delay = 0.5
-    url = ScanQRCode(r"10171030.png")
-    # url = "https://www.wjx.cn/vm/evfoZuA.aspx# "
+    # url = ScanQRCode(r"10171030.png")
+    url = "https://www.wjx.cn/vm/evfoZuA.aspx# "
     browser = selenium.webdriver.Chrome()
     browser.get(url)
     while True:
