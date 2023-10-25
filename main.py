7686968a8f92957b29f3121a3877b839a8f48d20
CrushFxl <2428977170@qq.com>
2023-10-25 23:44:43 +0800

    重构 添加对象WebTask

--- main.py
+++ main.py
@@ -1,25 +1,55 @@
 # coding=utf-8
 
 import time
+import os
+import re
 import selenium
 from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
 from cv2 import imread, QRCodeDetector
 from configparser import ConfigParser
 
 cnt = 1
 
 
+class WebTask:
+    def __init__(self, img):
+        self.browser = selenium.webdriver.Chrome()
+        self.url = self.ScanQRCode(img)
+        self.name = None
+        self.time = None
+        self.state = None
+        self.GetBasicInfo()
+
+    def GetBasicInfo(self):
+        self.browser.get(self.url)
+        page = self.browser.page_source
+        self.name = re.search(r"<title>(.*?)</title>", page).groups()[0]
+        matchOld = re.search(r"不能再接受新的答卷", page)
+        matchTime = re.search(r"距离开始还有(\d+)天(\d+)时(\d+)分(\d+)秒", page)
+        if matchOld:
+            self.state = -1
+        if matchTime:
+            d, h, m, s = matchTime.groups()
+            self.time = int(d) * 86400 + int(h) * 3600 + int(m) * 60 + int(s)
+            self.name = re.search(r"\"htitle\">(.*?)<", page).groups()[0]
+            self.state = 0
+        else:
+            self.state = 1
+        print(self.name)
+
+    @staticmethod
+    def ScanQRCode(file_name):
+        img = imread("./task/" + file_name)
+        det = QRCodeDetector()
+        url, pts, st_code = det.detectAndDecode(img)
+        return url
+
+
 def ReadSetting():
     conf = ConfigParser()
     conf.read('config.ini', encoding='utf-8')
     return float(conf['setting']['delay'])
-
-
-def ScanQRCode(path):
-    img = imread(path)
-    det = QRCodeDetector()
-    info, pts, st_code = det.detectAndDecode(img)
-    return info
 
 
 def MatchAnswer(text):
@@ -49,24 +79,28 @@ def FillForm(que):
 
 
 if __name__ == "__main__":
-    url = ScanQRCode(r"img.png")
     delay = ReadSetting()
+    chrome_options = Options()
+    chrome_options.page_load_strategy = 'eager'
+    path = os.getcwd() + "/task/"  # 程序下绝对路径
+    imgs = os.listdir(path)  # 文件名列表
+    for img in imgs:
+        t = WebTask(img)
+
+        # match = re.search(r"不能再接受新的答卷", browser.page_source)
+
     #  url = "https://www.wjx.cn/vm/evfoZuA.aspx#"
-    browser = selenium.webdriver.Chrome()
-    browser.get(url)
-    while True:
-        ques = browser.find_elements(By.CSS_SELECTOR, '[class="field ui-field-contain"]')
-        if len(ques) == 0:
-            print(f"【WARNING】无可填项，问卷可能未开放。 #{cnt}")
-            cnt += 1
-            time.sleep(delay)
-            browser.refresh()
-            continue
-        for que in ques:
-            FillForm(que)
-        browser.find_element(By.CSS_SELECTOR, '[class="submitbtn mainBgColor"]').click()
-        break
-    print("【INFO】计划任务已执行，60秒后将关闭网页...")
-    print("Github开源项目地址：https://github.com/CrushFxl/FormSubmitTool-based-on-wjx")
-    print("作者：杭医CrushFxl 程序仅供学习交流使用，切勿作其他用途。")
-    time.sleep(60)
+    # while True:
+    #     ques = browser.find_elements(By.CSS_SELECTOR, '[class="field ui-field-contain"]')
+    #     if len(ques) == 0:
+    #         time.sleep(delay)
+    #         browser.refresh()
+    #         continue
+    #     for que in ques:
+    #         FillForm(que)
+    #     browser.find_element(By.CSS_SELECTOR, '[class="submitbtn mainBgColor"]').click()
+    #     break
+    # print("【INFO】计划任务已执行，60秒后将关闭网页...")
+    # print("Github开源项目地址：https://github.com/CrushFxl/FormSubmitTool-based-on-wjx")
+    # print("作者：杭医CrushFxl 程序仅供学习交流使用，切勿作其他用途。")
+    # time.sleep(60)
